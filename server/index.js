1c5dabd45cd4da109004f43388789ce2296031be
wildfirez98 <james@areafamily.us>
2022-05-02 21:13:01 -0500

    Added .env file!

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
+// Modules and Globals
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL="mongodb+srv://wildfirez98:Betasig98@cluster0.rdrrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT 
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
     .catch ((error) => console.log(error.message));
 
