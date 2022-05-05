769f988eab32554acc5f07cc91a953b8f57d05ec
Benjamin Banwart <benjibanwart@gmail.com>
2022-05-04 20:50:14 -0500

    sql progress

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
+app.get("/", (req, res) => {
+    res.send('Welcome to MERNstagram')
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Live on port: ${process.env.PORT}`)
 })
