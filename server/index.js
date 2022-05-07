f79b0e488158463acbf66b6b79de50a22c19085b
Benjamin Banwart <benjibanwart@gmail.com>
2022-05-07 11:42:43 -0500

    hid config string

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { Sequelize } = require('sequelize')
+// const { Sequelize } = require('sequelize')
 
 
 require('dotenv').config();
