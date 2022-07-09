913b40a923da1d7c5c235a549e15a3294a492a53
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2022-07-09 13:12:03 -0500

    prepped for aws deployment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+require('dotenv').config()
 const methodOverride = require('method-override')
 const cors = require('cors')
 const app = express()
+const path = require('path')
 // const { Sequelize } = require('sequelize')
 
 
@@ -10,6 +12,11 @@ app.use(methodOverride('_method'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
+
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
 
 app.get('/', (req, res) => {
     res.status(200).json({
